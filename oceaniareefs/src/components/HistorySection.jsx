7af97a1a2dbb95cadae4aff4bb4205f345dae53a
bstrong2017 <bstrong2017@fau.edu>
2025-10-27 21:15:12 -0400

    added graphs and more sections

--- oceaniareefs/src/components/HistorySection.jsx
+++ oceaniareefs/src/components/HistorySection.jsx
@@ -1,5 +1,6 @@
 // src/components/HistorySection.jsx
 import React from "react";
+// import { Image, View, StyleSheet } from 'react-native';
 
 export default function HistorySection() {
   return (
@@ -17,7 +18,44 @@ export default function HistorySection() {
         species, the structure of coral reefs protects coastal communities.
       </p>
       <h2>Data Methods</h2>
-      <p>put data here</p>
+      <p>Imported and cleaned Kaggle’s Global Bleaching Environmental dataset (limited to Oceania → 
+        2,956 entries).</p>
+      <p>Focused on 6 key coral reef
+        regions in Australia with high
+        bleaching rates.</p>
+      <div>
+      <ul className="right-align">
+          <li><strong>Darwin north Australia</strong></li>
+          <li><strong>Scott Reef west Australia</strong></li>
+          <li><strong>Kimberley Coast north-west Australia</strong></li>
+          <li><strong>Southern Great Barrier Reef</strong></li>
+          <li><strong>Pompey and Swain Reefs</strong></li>
+          <li><strong>South-east Great Barrier</strong></li>
+          <li><strong>Torres Strait and far northern Great Barrier Reef</strong></li>
+        </ul>
+      </div>
+      <h2>Discovery</h2>
+      <p>Coral Bleaching Trends ​
+        Strong correlation: climate temperature peaks → bleaching spikes​
+        Other contributing factors: windspeed,turbidity, thermal anomalies​
+        Sudden peaks (1998, 2002, 2016) showbleaching is non-linear, not constant</p>
+      <h2>Innovative Solutions</h2>
+      <h2>Looking Ahead</h2>
+      <p>Continue to expand and update data sources.​
+        Continue to refine models with new data.​
+        Implementation of real-time early monitoring systems.​
+      Collaboration among different conservation organizations.​
+      Promotion of bleaching awareness to the general public.​
+      Establish a Universal strategy for Coral Reef conservation.</p>
+      <h2>Links</h2>
+      <p>Global Bleaching Environment Data set: https://www.kaggle.com/datasets/pnminh95/global-bleaching-environmental</p>
+      <p>Github: </p>
+      <h2>References</h2>
+      <p>pnminh95. (n.d.). Global bleaching environmental [Data set]. Kaggle. Global Bleaching Environmental dataset </p>
+      <p>Rosane, O. (2021). 98% of the Great Barrier Reef has been affected by coral bleaching. World Economic Forum. World Economic Forum.     </p>
+      <p classname = "align-text-right">(n.d.). Great Barrier Reef Timeline. GBR Biology. GBR Biology .</p>
+      <p>Yeung, J.(2025). Australia’s Great Barrier Reef devastated by worst coral bleaching on record, new report finds. CNN Climate. CNN.</p>
     </section>
+    
   );
 }
